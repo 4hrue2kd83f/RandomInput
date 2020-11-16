27e903a25e0480b9e3abc1810e0f9f3d83c06693
4hrue2kd83f <ccai2271@gmail.com>
2020-11-16 13:10:59 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,207 +1,68 @@
-let capitalizeNext = true;
-let spaceAfter = false;
-let glue = false;
 let currentWord = "";
-let history = [""];
-let numbers = /^\d+/g;
+let counter = 0;
 
 clear.onclick = () => {
     textArea.value = "";
 }
 
 enter.onclick = () => {
-    if (S.value == "S") {
-        if (num.value == "#") {
-            currentWord += "1";
-        } else {
-            currentWord += "S";
-        }
+    if (confirm("Enter this word: " + currentWord + "?")) {
+        textArea.value += currentWord;
     }
-    if (T.value == "T") {
-        if (num.value == "#") {
-            currentWord += "2";
-        } else {
-            currentWord += "T";
-        }
-    }
-    if (K.value == "K") {
-        currentWord += "K";
-    }
-    if (P.value == "P") {
-        if (num.value == "#") {
-            currentWord += "3";
-        } else {
-            currentWord += "P";
-        }
-    }
-    if (W.value == "W") {
-        currentWord += "W";
-    }
-    if (H.value == "H") {
-        if (num.value == "#") {
-            currentWord += "4";
-        } else {
-            currentWord += "H";
-        }
-    }
-    if (R.value == "R") {
-        currentWord += "R";
-    }
-    if (A.value == "" && O.value == "" && star.value == "" && E.value == "" && U.value == "" && num.value == "") {
-        currentWord += "-";
-    }
-    if (A.value == "A") {
-        if (num.value == "#") {
-            currentWord += "5";
-        } else {
-            currentWord += "A";
-        }
-    }
-    if (O.value == "O") {
-        if (num.value == "#") {
-            currentWord += "0";
-        } else {
-            currentWord += "O";
-        }
-    }
-    if (star.value == "*") {
-        currentWord += "*";
-    }
-    if (E.value == "E") {
-        currentWord += "E";
-    }
-    if (U.value == "U") {
-        currentWord += "U";
-    }
-    if (f.value == "F") {
-        if (num.value == "#") {
-            currentWord += "6";
-        } else {
-            currentWord += "F";
-        }
-    }
-    if (r.value == "R") {
-        currentWord += "R";
-    }
-    if (p.value == "P") {
-        if (num.value == "#") {
-            currentWord += "7";
-        } else {
-            currentWord += "P";
-        }
-    }
-    if (b.value == "B") {
-        currentWord += "B";
-    }
-    if (l.value == "L") {
-        if (num.value == "#") {
-            currentWord += "8";
-        } else {
-            currentWord += "L";
-        }
-    }
-    if (g.value == "G") {
-        currentWord += "G";
-    }
-    if (t.value == "T") {
-        if (num.value == "#") {
-            currentWord += "9";
-        } else {
-            currentWord += "T";
-        }
-    }
-    if (s.value == "S") {
-        currentWord += "S";
-    }
-    if (d.value == "D") {
-        currentWord += "D";
-    }
-    if (z.value == "Z") {
-        currentWord += "Z";
-    }
-    if (currentWord.charAt(currentWord.length - 1) == "-") {
-        currentWord = currentWord.slice(0,-1);
-    }
-
-    ////////////////////////////////
-
-    if (stroke.value == "/") {
-        currentWord += "/";
-    } else {
-        word = stenoData[currentWord];
-        hasSpace = false;
-        if (word == undefined) {
-            word = currentWord;
-        }
-        if (spaceAfter == true) {
-            hasSpace = true;
-        }
-        spaceAfter = true;
-        word = word.replace("{}", "");
-        if (word.includes("{-|}")) {
-            hasSpace = false;
-            capitalizeNext = true;
-            word = word.replace("{-|}", "");
-        }
-        if (word.includes("{>}")) {
-            hasSpace = false;
-            capitalizeNext = false;
-            word = word.replace("{>}", "");
-        }
-        console.log(numbers.test(word));
-        if (numbers.test(word) == true) {
-            if (glue == true) {
-                hasSpace = false;
-            }
-            glue = true;
-        } else if (word.charAt(0) == "{" && word.charAt(1) == "&" && word.charAt(word.length - 1) == "}") {
-            word = word.substring(2, word.length - 1);
-            if (glue == true) {
-                hasSpace = false;
-            }
-            glue = true;
-        } else {
-            glue = false;
-        }
-        if (word.charAt(0) == "{" && word.charAt(word.length - 1) == "}") {
-            word = word.substring(1, word.length - 1);
-            hasSpace = false;
-        }
-        if (word.includes("{^.^}")) {
-            word = word.replace("{^.^}", ".");
-            hasSpace = false;
-        }
-        if (word.charAt(0) == "^") {
-            hasSpace = false;
-            word = word.substring(1, word.length);
-        }
-        if (word.charAt(word.length - 1) == "^") {
-            spaceAfter = false;
-            word = word.substring(0, word.length - 1)
-        }
-        word = word.replace(">}{&", "");
-        if (capitalizeNext == true) {
-            word = word.charAt(0).toUpperCase() + word.slice(1)
-        }
-        capitalizeNext = false;
-        if (hasSpace == true) {
-            textArea.value += " ";
-        }
-        spaceAfter = true;
-        switch (word) {
-            case ".":
-            case "?":
-            case "!":
-            case ":":
-                capitalizeNext = true;
-        }
-        textArea.value += word;
-        currentWord = "";
-    }
-
 }
 changePos();
 
+newword.onclick = () => {
+    if (confirm("Are you sure you want to get a new word?")) {
+        currentWord = wordlist[Math.floor(Math.random() * wordlist.length)];
+        document.getElementById("selectedWord").innerHTML = currentWord;
+        counter++;
+    }
+}
+
+space.onclick = () => {
+    if (confirm("Are you sure you want to add a space?")) {
+        textArea.value += " ";
+    }
+}
+
+tkls.onclick = () => {
+    if (confirm("Are you sure you want to remove a space?\n\nNOTE: If you didn't just add a space, there will be serious consequences!")) {
+        if (textArea.value.charAt(textArea.value.length - 1) == " ") {
+            textArea.value = textArea.value.slice(0, -1);
+        } else {
+            alert("This isn't a backspace button, you hacker. To avoid further hacking, the text box will now be cleared.");
+            textArea.value = "";
+        }
+    }
+}
+
+uppercase.onclick = () => {
+    if (confirm("Are you sure you want to capitalize this word?")) {
+        currentWord = currentWord.charAt(0).toUpperCase() + currentWord.slice(1)
+        document.getElementById("selectedWord").innerHTML = currentWord;
+    }
+}
+
+lowercase.onclick = () => {
+    if (confirm("Are you sure you want to lowercase this word?")) {
+        currentWord = currentWord.charAt(0).toUpperCase() + currentWord.slice(1)
+        document.getElementById("selectedWord").innerHTML = currentWord;
+    }
+}
+
+newline.onclick = () => {
+    if (confirm("Are you sure you want to add a new line?")) {
+        textArea.value += "\n";
+    }
+}
+
+postbutton.onclick = () => {
+    if (confirm("Posting must be unlocked by purchasing Premium for $4.99. Please click \"OK\" to proceed to checkout.")) {
+        prompt("Please enter your credit card number.", "0000 0000 0000 0000");
+        alert("Invalid credit card number. Purchase has been cancelled.");
+    }
+}
 
 function changePos() {
     /*     t.focus();
@@ -209,19 +70,6 @@ function changePos() {
         s.style.left = getCursorXY(t, t.selectionEnd).x - 100 + "px";
         t.blur();
      */
-}
-
-post.onclick = () => {
-    const posting = document.createElement("span");
-    posting.style.display = "block";
-    posting.style.marginTop = "0.5em";
-    posting.style.fontSize = "1.3em";
-    posting.innerText = "Posting...";
-    document.body.appendChild(posting);
-
-    setTimeout(() => {
-        posting.innerText = "Posted!";
-    }, 1500);
 }
 
 
